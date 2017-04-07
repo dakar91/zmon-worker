c240d2222fc7a84a71cce92eee95e1aa41586c93
dakar91 <dakar2889@gmail.com>
2017-04-07 14:41:42 +0100

    New features for the Data Pipeline wrapper.

--- tests/test_datapipeline.py
+++ tests/test_datapipeline.py
@@ -2,78 +2,221 @@ from zmon_worker_monitor.builtins.plugins.datapipeline import DataPipelineWrappe
 
 from mock import MagicMock
 
+list_pipelines_result = {
+  'pipelineIdList': [
+    {u'id': u'df-00627471SOVYZEXAMPLE', 'name': 'my-pipeline'},
+    {u'id': u'df-00627471SOFDLKASJDDF', 'name': 'my-pipeline2'},
+    {u'id': u'df-0870198233ZYVEXAMPLE', 'name': 'CrossRegionDDB'},
+    {u'id': u'df-00189603TB4MZEXAMPLE', 'name': 'CopyRedshift'}
+  ]
+}
 
-def test_get_details_one_pipeline(monkeypatch):
+describe_pipelines_result_single = {
+  'pipelineDescriptionList': [
+    {
+      'fields': [
+        {u'stringValue': 'PENDING', 'key': '@pipelineState'},
+        {u'stringValue': 'my-pipeline', 'key': 'name'},
+        {'stringValue': 'my-pipeline-token', 'key': 'uniqueId'}
+      ],
+      'pipelineId': 'df-00627471SOVYZEXAMPLE',
+      'name': 'my-pipeline',
+      'tags': []
+    }
+  ]
+}
+
+describe_pipelines_result_multiple = {
+  'pipelineDescriptionList': [
+    {
+      'fields': [
+        {u'stringValue': 'PENDING', 'key': '@pipelineState'},
+        {u'stringValue': 'my-pipeline', 'key': 'name'},
+        {'stringValue': 'my-pipeline-token', 'key': 'uniqueId'}
+      ],
+      'pipelineId': 'df-00627471SOVYZEXAMPLE',
+      'name': 'my-pipeline',
+      'tags': []
+    },
+    {
+      'fields': [
+        {u'stringValue': 'SCHEDULED', 'key': '@pipelineState'},
+        {u'stringValue': 'my-pipeline2', 'key': 'name'},
+        {'stringValue': 'my-pipeline-token2', 'key': 'uniqueId'}
+      ],
+      'pipelineId': 'df-00627471SOFDLKASJDDF',
+      'name': 'my-pipeline2',
+      'tags': []
+    }
+  ]
+}
+
+
+def test_list_pipelines_no_parameter(monkeypatch):
     client = MagicMock()
-    client.describe_pipelines.return_value = {'pipelineDescriptionList': [{'fields': [
-      {u'key': u'@cancelActive',
-       u'stringValue': u'true'},
-      {'key': '@id',
-       'stringValue': 'pipeline_1'},
-      {'key': '@accountId',
-       'stringValue': '123456789'}],
-      'name': 'pipeline_1',
-      'pipelineId': 'pipeline_1',
-      'tags': [{'key': 'DataPipelineName',
-                'value': 'pipeline_1'},
-               {'key': 'DataPipelineId',
-                'value': 'pipeline_1'}]}]}
+    client.list_pipelines.return_value = list_pipelines_result
 
     get = MagicMock()
     get.return_value.json.return_value = {'region': 'myregion'}
     monkeypatch.setattr('requests.get', get)
     monkeypatch.setattr('boto3.client', lambda x, region_name: client)
     datapipeline = DataPipelineWrapper()
-    elb_data = datapipeline.get_details('pipeline_1')
-    assert elb_data == {'pipeline_1': {'@accountId': '123456789',
-                                       '@cancelActive': 'true',
-                                       '@id': 'pipeline_1'}}
+    elb_data = datapipeline.list_pipelines()
+    assert elb_data == {'my-pipeline': 'df-00627471SOVYZEXAMPLE',
+                        'my-pipeline2': 'df-00627471SOFDLKASJDDF',
+                        'CrossRegionDDB': 'df-0870198233ZYVEXAMPLE',
+                        'CopyRedshift': 'df-00189603TB4MZEXAMPLE'}
 
-    client.describe_pipelines.assert_called_with(pipelineIds=['pipeline_1'])
+    client.list_pipelines.assert_called_with()
 
 
-def test_get_details_multiple_pipelines(monkeypatch):
+def test_list_pipelines_one_pipeline(monkeypatch):
     client = MagicMock()
-    client.describe_pipelines.return_value = {'pipelineDescriptionList': [{'fields': [
-      {'key': '@cancelActive',
-       'stringValue': 'true'},
-      {'key': '@id',
-       'stringValue': 'pipeline_1'},
-      {'key': '@accountId',
-       'stringValue': '123456789'}],
-      'name': 'pipeline_1',
-      'pipelineId': 'pipeline_1',
-      'tags': [{'key': 'DataPipelineName',
-                'value': 'pipeline_1'},
-               {'key': 'DataPipelineId',
-                'value': 'pipeline_1'}]},
-      {'fields': [{'key': '@sphere',
-                   'stringValue': 'PIPELINE'},
-                  {'key': '@healthStatus',
-                   'stringValue': 'HEALTHY'},
-                  {'key': '@id',
-                   'stringValue': 'pipeline_2'},
-                  {'key': '@pipelineState',
-                   'stringValue': 'SCHEDULED'}],
-       'name': 'pipeline_2',
-       'pipelineId': 'pipeline_2',
-       'tags': [{'key': 'ClusterName',
-                 'value': 'pipeline_2'},
-                {'key': 'DataPipelineId',
-                 'value': 'pipeline_2'}]}]}
+    client.list_pipelines.return_value = list_pipelines_result
 
     get = MagicMock()
     get.return_value.json.return_value = {'region': 'myregion'}
     monkeypatch.setattr('requests.get', get)
     monkeypatch.setattr('boto3.client', lambda x, region_name: client)
     datapipeline = DataPipelineWrapper()
-    elb_data = datapipeline.get_details(['pipeline_1', 'pipeline_2'])
-    assert elb_data == {'pipeline_2': {'@healthStatus': 'HEALTHY',
-                                       '@id': 'pipeline_2',
-                                       '@pipelineState': 'SCHEDULED',
-                                       '@sphere': 'PIPELINE'},
-                        'pipeline_1': {'@accountId': '123456789',
-                                       '@cancelActive': 'true',
-                                       '@id': 'pipeline_1'}}
+    elb_data = datapipeline.list_pipelines(pipeline_names='my-pipeline')
+    assert elb_data == {'my-pipeline': 'df-00627471SOVYZEXAMPLE'}
 
-    client.describe_pipelines.assert_called_with(pipelineIds=['pipeline_1', 'pipeline_2'])
+    client.list_pipelines.assert_called_with()
+
+
+def test_list_pipelines_multiple_pipelines(monkeypatch):
+    client = MagicMock()
+    client.list_pipelines.return_value = list_pipelines_result
+
+    get = MagicMock()
+    get.return_value.json.return_value = {'region': 'myregion'}
+    monkeypatch.setattr('requests.get', get)
+    monkeypatch.setattr('boto3.client', lambda x, region_name: client)
+    datapipeline = DataPipelineWrapper()
+    elb_data = datapipeline.list_pipelines(pipeline_names=['my-pipeline', 'my-pipeline2'])
+    assert elb_data == {'my-pipeline': 'df-00627471SOVYZEXAMPLE', 'my-pipeline2': 'df-00627471SOFDLKASJDDF'}
+
+    client.list_pipelines.assert_called_with()
+
+
+def test_describe_pipelines_one_pipeline(monkeypatch):
+    client = MagicMock()
+    client.describe_pipelines.return_value = describe_pipelines_result_single
+
+    get = MagicMock()
+    get.return_value.json.return_value = {'region': 'myregion'}
+    monkeypatch.setattr('requests.get', get)
+    monkeypatch.setattr('boto3.client', lambda x, region_name: client)
+    datapipeline = DataPipelineWrapper()
+    elb_data = datapipeline.describe_pipelines(pipeline_ids='df-00627471SOVYZEXAMPLE')
+    assert elb_data == {'df-00627471SOVYZEXAMPLE': {'name': 'my-pipeline',
+                                                    'uniqueId': 'my-pipeline-token',
+                                                    'pipelineState': 'PENDING'}}
+
+    client.describe_pipelines.assert_called_with(pipelineIds=['df-00627471SOVYZEXAMPLE'])
+
+
+def test_describe_pipelines_multiple_pipelines(monkeypatch):
+    client = MagicMock()
+    client.describe_pipelines.return_value = describe_pipelines_result_multiple
+
+    get = MagicMock()
+    get.return_value.json.return_value = {'region': 'myregion'}
+    monkeypatch.setattr('requests.get', get)
+    monkeypatch.setattr('boto3.client', lambda x, region_name: client)
+    datapipeline = DataPipelineWrapper()
+    elb_data = datapipeline.describe_pipelines(pipeline_ids=['df-00627471SOVYZEXAMPLE', 'df-00627471SOFDLKASJDDF'])
+    assert elb_data == {'df-00627471SOFDLKASJDDF': {'name': 'my-pipeline2',
+                                                    'uniqueId': 'my-pipeline-token2',
+                                                    'pipelineState': 'SCHEDULED'},
+                        'df-00627471SOVYZEXAMPLE': {'name': 'my-pipeline',
+                                                    'uniqueId': 'my-pipeline-token',
+                                                    'pipelineState': 'PENDING'}}
+
+    client.describe_pipelines.assert_called_with(pipelineIds=['df-00627471SOFDLKASJDDF', 'df-00627471SOVYZEXAMPLE'])
+
+
+def test_describe_pipelines_multiple_pipelines_with_names_ids(monkeypatch):
+    client = MagicMock()
+    client.describe_pipelines.return_value = describe_pipelines_result_multiple
+    client.list_pipelines.return_value = list_pipelines_result
+
+    get = MagicMock()
+    get.return_value.json.return_value = {'region': 'myregion'}
+    monkeypatch.setattr('requests.get', get)
+    monkeypatch.setattr('boto3.client', lambda x, region_name: client)
+    datapipeline = DataPipelineWrapper()
+    elb_data = datapipeline.describe_pipelines(pipeline_ids=['df-00627471SOVYZEXAMPLE'], pipeline_names='my-pipeline2')
+    assert elb_data == {'df-00627471SOFDLKASJDDF': {'name': 'my-pipeline2',
+                                                    'uniqueId': 'my-pipeline-token2',
+                                                    'pipelineState': 'SCHEDULED'},
+                        'df-00627471SOVYZEXAMPLE': {'name': 'my-pipeline',
+                                                    'uniqueId': 'my-pipeline-token',
+                                                    'pipelineState': 'PENDING'}}
+
+    client.list_pipelines.assert_called_with()
+    client.describe_pipelines.assert_called_with(pipelineIds=['df-00627471SOFDLKASJDDF', 'df-00627471SOVYZEXAMPLE'])
+
+
+def test_describe_pipelines_single_pipeline_names(monkeypatch):
+    client = MagicMock()
+    client.describe_pipelines.return_value = describe_pipelines_result_single
+    client.list_pipelines.return_value = list_pipelines_result
+
+    get = MagicMock()
+    get.return_value.json.return_value = {'region': 'myregion'}
+    monkeypatch.setattr('requests.get', get)
+    monkeypatch.setattr('boto3.client', lambda x, region_name: client)
+    datapipeline = DataPipelineWrapper()
+    elb_data = datapipeline.describe_pipelines(pipeline_names='my-pipeline')
+    assert elb_data == {'df-00627471SOVYZEXAMPLE': {'name': 'my-pipeline',
+                                                    'uniqueId': 'my-pipeline-token',
+                                                    'pipelineState': 'PENDING'}}
+
+    client.list_pipelines.assert_called_with()
+    client.describe_pipelines.assert_called_with(pipelineIds=['df-00627471SOVYZEXAMPLE'])
+
+
+def test_describe_pipelines_multiple_pipelines_duplicate_names_ids(monkeypatch):
+    client = MagicMock()
+    client.describe_pipelines.return_value = describe_pipelines_result_single
+    client.list_pipelines.return_value = list_pipelines_result
+
+    get = MagicMock()
+    get.return_value.json.return_value = {'region': 'myregion'}
+    monkeypatch.setattr('requests.get', get)
+    monkeypatch.setattr('boto3.client', lambda x, region_name: client)
+    datapipeline = DataPipelineWrapper()
+    elb_data = datapipeline.describe_pipelines(pipeline_ids=['df-00627471SOVYZEXAMPLE'], pipeline_names=['my-pipeline'])
+    assert elb_data == {'df-00627471SOVYZEXAMPLE': {'name': 'my-pipeline',
+                                                    'uniqueId': 'my-pipeline-token',
+                                                    'pipelineState': 'PENDING'}}
+
+    client.list_pipelines.assert_called_with()
+    client.describe_pipelines.assert_called_with(pipelineIds=['df-00627471SOVYZEXAMPLE'])
+
+
+def test_describe_all_pipelines(monkeypatch):
+    client = MagicMock()
+    client.describe_pipelines.return_value = describe_pipelines_result_multiple
+    client.list_pipelines.return_value = list_pipelines_result
+
+    get = MagicMock()
+    get.return_value.json.return_value = {'region': 'myregion'}
+    monkeypatch.setattr('requests.get', get)
+    monkeypatch.setattr('boto3.client', lambda x, region_name: client)
+    datapipeline = DataPipelineWrapper()
+    elb_data = datapipeline.describe_all_pipelines()
+
+    # there should be more items here but it's enough for the purpose of the test
+    assert elb_data == {'df-00627471SOFDLKASJDDF': {'name': 'my-pipeline2',
+                                                    'uniqueId': 'my-pipeline-token2',
+                                                    'pipelineState': 'SCHEDULED'},
+                        'df-00627471SOVYZEXAMPLE': {'name': 'my-pipeline',
+                                                    'uniqueId': 'my-pipeline-token',
+                                                    'pipelineState': 'PENDING'}}
+
+    client.list_pipelines.assert_called_with()
+    client.describe_pipelines.assert_called_with(pipelineIds=['df-00189603TB4MZEXAMPLE', 'df-00627471SOFDLKASJDDF',
+                                                              'df-00627471SOVYZEXAMPLE', 'df-0870198233ZYVEXAMPLE'])
--- zmon_worker_monitor/builtins/plugins/datapipeline.py
+++ zmon_worker_monitor/builtins/plugins/datapipeline.py
@@ -5,7 +5,6 @@ import boto3
 import logging
 import requests
 
-
 from zmon_worker_monitor.zmon_worker.errors import CheckError
 from zmon_worker_monitor.adapters.ifunctionfactory_plugin import IFunctionFactoryPlugin, propartial
 
@@ -38,7 +37,7 @@ def get_region():
 def create_dict_from_list_of_fields(fields):
     fields_dict = {}
     for field in fields:
-        fields_dict[str(field['key'])] = str(field['stringValue'])
+        fields_dict[str(field['key']).replace('@', '')] = str(field['stringValue'])
 
     return fields_dict
 
@@ -49,23 +48,69 @@ class DataPipelineWrapper(object):
             region = get_region()
         self.__client = boto3.client('datapipeline', region_name=region)
 
-    def get_details(self, pipeline_ids):
+    def list_pipelines(self, pipeline_names=None):
         """
-            Return a list of pipelines with their details.
+        List all the pipelines with their name and id.
+        :param pipeline_names: Pipeline names as a String or a list of Strings
+        :type pipeline_names: str or list
 
-            :param name: Pipeline IDs as a String for a single item or list of Strings for multiple pipelines.
-            :param type: str
-            :param type: list
-
-            :return: Details from the requested pipelines
-            :rtype: map
+        :return: All the pipelines for this AWS account.
+        :rtype: s/map/dict
         """
+
+        if pipeline_names is None:
+            pipeline_names = []
+
+        if isinstance(pipeline_names, str) or pipeline_names is None:
+            pipeline_names = [pipeline_names]
+        elif not isinstance(pipeline_names, list):
+                raise CheckError('Parameter "pipeline_names" should be a string or a list of strings '
+                                 'denoting pipeline names')
+
+        pipelines = self.__client.list_pipelines()
+        return {pipeline['name']: pipeline['id']
+                for pipeline in pipelines['pipelineIdList']
+                if not pipeline_names or pipeline['name'] in pipeline_names}
+
+    def describe_pipelines(self, pipeline_ids=None, pipeline_names=None):
+        """
+        Return a list of pipelines with their details.
+        :param pipeline_ids: Pipeline IDs as a String or list of Strings.
+        :type pipeline_ids: str or list
+
+        :param pipeline_names: Pipeline names as a String or list of Strings
+        :type pipeline_names: str or list
+
+        :return Details from the requested pipelines
+        :rtype: s/map/dict
+        """
+
+        if pipeline_ids is None:
+            pipeline_ids = []
+
         if isinstance(pipeline_ids, str):
             pipeline_ids = [pipeline_ids]
-        else:
-            if not isinstance(pipeline_ids, list):
-                raise CheckError('Parameter \"pipeline_ids\" should be a string or a list of strings '
+        elif not isinstance(pipeline_ids, list):
+                raise CheckError('Parameter "pipeline_ids" should be a string or a list of strings '
                                  'denoting pipeline IDs')
+
+        if pipeline_names is None:
+            pipeline_names = []
+
+        if isinstance(pipeline_names, str):
+            pipeline_names = [pipeline_names]
+        elif not isinstance(pipeline_names, list):
+                raise CheckError('Parameter "pipeline_names" should be a string or a list of strings '
+                                 'denoting pipeline names')
+
+        if not pipeline_ids and not pipeline_names:
+            pipeline_ids = self.list_pipelines().values()
+        elif pipeline_names:
+            # add all the IDs retrieved from their names
+            pipeline_ids.extend(self.list_pipelines(pipeline_names).values())
+
+        # make sure we don't have duplicate IDs *sorted is for testing purposes*
+        pipeline_ids = sorted(list(set(pipeline_ids)))
 
         response = self.__client.describe_pipelines(pipelineIds=pipeline_ids)
 
@@ -83,3 +128,11 @@ class DataPipelineWrapper(object):
 
         # returns a map which has the pipeline IDs as keys and their details as values
         return result
+
+    def describe_all_pipelines(self):
+        """
+        Return a list of pipelines with their details.
+        :return: Details from all the pipelines
+        :rtype: s/map/dict
+        """
+        return self.describe_pipelines(pipeline_ids=self.list_pipelines().values())
